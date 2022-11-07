a308d12432e3c137cd7d68296488fb987283642b
nonsuch1 <nonsuch.11111@gmail.com>
2022-11-07 13:00:22 +0300

    121. Best Time to Buy and Sell Stock
    Runtime 2 ms Beats 93.17%
    Memory 83.5 MB Beats 51.70%

--- src/com/nonsuch1/slidingWindow/BestTimeToBuyAndSellStock.java
+++ src/com/nonsuch1/slidingWindow/BestTimeToBuyAndSellStock.java
@@ -2,10 +2,10 @@ package com.nonsuch1.slidingWindow;
 
 /**
  * You are given an array prices where prices[i] is the price of a given stock on the ith day.
- *
+ * <p>
  * You want to maximize your profit by choosing a single day to buy one stock and choosing
  * a different day in the future to sell that stock.
- *
+ * <p>
  * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
  */
 public class BestTimeToBuyAndSellStock {
@@ -18,16 +18,14 @@ public class BestTimeToBuyAndSellStock {
     static class Solution {
         public int maxProfit(int[] prices) {
             int maxProfit = 0;
+            int minPrice = Integer.MAX_VALUE;
             for (int i = 0; i < prices.length; i++) {
-                int buyPrice = prices[i];
-                int sellPrice = buyPrice;
-                for (int j = i + 1; j < prices.length; j++) {
-                    if (prices[j] > sellPrice) {
-                        sellPrice = prices[j];
-                    }
+                if (prices[i] < minPrice) {
+                    minPrice = prices[i];
                 }
-                if (sellPrice - buyPrice > maxProfit) {
-                    maxProfit = sellPrice - buyPrice;
+                int profit = prices[i] - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
                 }
             }
             return maxProfit;
