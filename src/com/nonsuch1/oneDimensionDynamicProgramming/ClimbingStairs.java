4c7b93ab00e027d7e6fb2fc1c6f539e532086fdb
nonsuch1 <nonsuch.11111@gmail.com>
2022-11-14 12:16:34 +0300

    70. Climbing Stairs
    Needs to be refactored using array or a map.

---
+++ src/com/nonsuch1/oneDimensionDynamicProgramming/ClimbingStairs.java
@@ -0,0 +1,53 @@
+package com.nonsuch1.oneDimensionDynamicProgramming;
+
+
+/**
+ * 70. Climbing Stairs
+
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ *
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 45
+ *
+ */
+public class ClimbingStairs {
+
+    static class Solution {
+        public int climbStairs(int n) {
+            int n1 = 1;
+            int n2 = 2;
+            if (n1 == n)
+                return n1;
+            if (n2 == n)
+                return n2;
+            int index = 3;
+            int steps = n1 + n2;
+            while (index < n) {
+                n1 = n2;
+                n2 = steps;
+                steps = n1 + n2;
+                index++;
+            }
+            return steps;
+        }
+    }
+}
