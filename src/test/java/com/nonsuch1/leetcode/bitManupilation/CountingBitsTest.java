18dcbdd476729985aaa24c20d86705a1a235a83c
nonsuch1 <nonsuch.11111@gmail.com>
2022-11-21 17:03:02 +0300

    338. CountingBits
    Test

---
+++ src/test/java/com/nonsuch1/leetcode/bitManupilation/CountingBitsTest.java
@@ -0,0 +1,17 @@
+package com.nonsuch1.leetcode.bitManupilation;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CountingBitsTest {
+    CountingBits countingBits = new CountingBits();
+
+    @Test
+    void countBits() {
+        int[] expected = new int[]{0, 1, 1, 2, 1, 2};
+
+        int[] actual = countingBits.countBits(5);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
